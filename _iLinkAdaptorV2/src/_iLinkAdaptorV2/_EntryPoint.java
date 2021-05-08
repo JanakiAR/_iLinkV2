aa8a3ffd81e0df58da479f2c5eb2f88f206cef83
91789 <91789@DESKTOP-HDPMKPN>
2021-05-08 11:56:59 +0530

    First Commit

---
+++ _iLinkAdaptorV2/src/_iLinkAdaptorV2/_EntryPoint.java
@@ -0,0 +1,10 @@
+package _iLinkAdaptorV2;
+
+public class _EntryPoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello....");
+	}
+
+}
